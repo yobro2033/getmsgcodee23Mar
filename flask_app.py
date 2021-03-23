5efe8a7aa1fda28c0946cad50b8d31c6647a9570
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-24 00:33:16 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,14 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'home page'
+def msg():
+    msg = request.args.get('msg', '')
+    f = open("file.txt", "w")
+    f.write(msg)
+    f.close()
+    return render_template('msg.html')
+return app.render_template(generate(), mimetype='text/plain')
 
-@app.route('/form')
-def form():
-    name = request.args.get('n', '')
-    if name == '':
-        return render_template('simple_form.html')
-    else:
-        return 'Hello ' + name
+@app.route('/read')
+def red():
+    f = open('file.txt', 'r')
+    return f.read
+
+
         
