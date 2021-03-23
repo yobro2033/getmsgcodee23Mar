0d20980751fe70b2759d744752d05e09dbc65687
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-23 23:53:28 +0700

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 def hello_name():
     name = request.args.get('n', '')
     return 'Hello ' + name
+
+@app.route('/form')
+def form():
+    if name == '':
+        return render_template('simple_form.html')
+    else:
+        return 'Hello ' + name
