973b40244b17a636cfa644571fe1532525436ff4
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-23 23:56:11 +0700

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -6,9 +6,3 @@ def hello_name():
     name = request.args.get('n', '')
     return 'Hello ' + name
 
-@app.route('/form')
-def form():
-    if name == '':
-        return render_template('simple_form.html')
-    else:
-        return 'Hello ' + name
