510dbb7ecb5ec681e4810d64d08786533bbe5024
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-24 00:52:44 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,16 +2,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+@app.route('/')
+def msg():
+    return render_template('index.html')
+
 @app.route('/read')
 def read():
     msg = request.args.get('msg', '')
     if msg != '':
         f = open("file.txt", "a")
-        f.write(msg)
+        f.write(msg + <br/>)
         f.close()
     f = open("file.txt", "r")
     return f.read()
 
-@app.route('/')
-def msg():
-    return render_template('index.html')
