51dcd49d29753941cb8ac4e1dacea4af6edf0bf9
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-03-23 23:42:18 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Viet!'
+def name_home():
+    name = request.args.get('n', '')
+    return 'Hello ' + name
